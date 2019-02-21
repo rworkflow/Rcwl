90f7140162a73c29d4448f75c23c89fd0cd58b06
Qiang Hu <qiang.hu@roswellpark.org>
2019-02-21 10:46:59 -0500

    Add cwltool version check

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+.onLoad <- function(libname, pkgname) {
+    cwltool <- system("which cwltool", intern = TRUE)
+    if(length(cwltool) > 0){
+        cwlversion <- system("cwltool --version", intern = TRUE)
+        cwlversion <- as.numeric(sub('.* 1.', "", cwlversion))
+        if(cwlversion < 0.2018){
+            stop("You cwltool is out of date, please update it!")
+        }
+    }else{
+        stop("cwltool is not found, ",
+             "Please install cwltool first!\n",
+             "https://github.com/common-workflow-language/cwltool#install")
+    }
+    
+    invisible()
+}
