1c7fba13480fc8457987bf6080984905835641df
Qiang Hu <qiang.hu@roswellpark.org>
2019-02-21 10:48:40 -0500

    fix typo

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
         cwlversion <- system("cwltool --version", intern = TRUE)
         cwlversion <- as.numeric(sub('.* 1.', "", cwlversion))
         if(cwlversion < 0.2018){
-            stop("You cwltool is out of date, please update it!")
+            stop("Your cwltool is out of date, please update it!")
         }
     }else{
         stop("cwltool is not found, ",
