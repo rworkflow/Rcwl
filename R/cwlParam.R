0a1cf323ba5cf69ddf63c3cae3164bdc6b6d0029
Qiang Hu <qiang.hu@roswellpark.org>
2019-09-26 10:39:51 -0400

    fix bug for array types

--- R/cwlParam.R
+++ R/cwlParam.R
@@ -151,6 +151,14 @@ inputs <- function(cwl) cwl@inputs
         }else{
             v <- value
         }
+    }else if(is(itype, "list")){
+        if(file_test("-d", value)) {
+            v <- list(class = "Directory", path = normalizePath(value))
+        }else if(file_test("-f", value)){
+            v <- list(class = "File", path = normalizePath(value))
+        }else{
+            v <- value
+        }
     }
     x@inputs[[name]]@value <- v
     x
@@ -269,6 +277,9 @@ setMethod(show, "InputParamList", function(object) {
             cat("    prefix: ",
                 object[[i]]@type@inputBinding$prefix, " ",
                 paste(unlist(v), collapse=" "), "\n", sep = "")
+        }else if(is(object[[i]]@type, "list")){
+            cat(" ", iname, " (", paste0(unlist(object[[i]]@type),
+                                          collapse = "|"), "): \n")
         }
     })
 })
