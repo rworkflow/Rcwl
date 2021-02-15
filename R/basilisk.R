fb5848badf62f9cd9fe661c277e537eba61483c8
Qian Liu <qliu7@buffalo.edu>
2021-02-15 13:05:19 -0500

    basilisk conda version updates

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,3 +1,4 @@
 #' @importFrom basilisk BasiliskEnvironment
-env_Rcwl <- basilisk::BasiliskEnvironment("env_Rcwl", pkgname="Rcwl",
-                                packages = c("cwltool==3.0"))
+env_Rcwl <- basilisk::BasiliskEnvironment("env_Rcwl", pkgname="basilisk",
+                                          packages = "python=3.8",
+                                          pip = c("cwltool==3.0.20210124104916"))
