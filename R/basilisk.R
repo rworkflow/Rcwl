43dc950e45b2e61397303d64f6a6902209258f7a
hubentu <qiang.hu@roswellpark.org>
2021-10-24 22:18:41 -0400

    update cwltool version

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,3 +1,3 @@
 #' @importFrom basilisk BasiliskEnvironment
 env_Rcwl <- basilisk::BasiliskEnvironment("env_Rcwl", pkgname="basilisk",
-                                          packages = "cwltools==v3.1")
+                                          packages = "cwltool==v3.1")
