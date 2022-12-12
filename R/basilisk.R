0c3413c93d78d301bfb70c3b2446d704aadb7335
hubentu <qiang.hu@roswellpark.org>
2022-12-12 15:37:17 -0500

    export env_Rcwl

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,3 +1,7 @@
+#' Rcwl conda environment
+#'
+#' Rcwl conda envrionment to install `cwltool` by basilisk.
 #' @importFrom basilisk BasiliskEnvironment
+#' @export
 env_Rcwl <- basilisk::BasiliskEnvironment("env_Rcwl", pkgname="Rcwl",
                                           packages = c("cwltool==3.1", "cwl-utils==0.19"))
